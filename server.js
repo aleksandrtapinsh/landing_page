f00dc1e76942c4b4168d5524d56818a2574a1fdd
Aleksandr Tapinsh <squrtle2003@gmail.com>
2026-05-20 21:54:20 -0400

    serving of images

--- server.js
+++ server.js
@@ -4,21 +4,32 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
+const MIME_TYPES = {
+  '.html': 'text/html',
+  '.css':  'text/css',
+  '.js':   'text/javascript',
+  '.png':  'image/png',
+  '.jpg':  'image/jpeg',
+  '.jpeg': 'image/jpeg',
+  '.gif':  'image/gif',
+  '.svg':  'image/svg+xml',
+  '.ico':  'image/x-icon',
+};
+
 const server = http.createServer((req, res) => {
-  if (req.url === '/' || req.url === '/index.html') {
-    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
-      if (err) {
-        res.writeHead(500);
-        res.end('Internal Server Error');
-        return;
-      }
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(data);
-    });
-  } else {
-    res.writeHead(404);
-    res.end('Not Found');
-  }
+  const urlPath = req.url === '/' ? '/index.html' : req.url;
+  const filePath = path.join(__dirname, urlPath);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(err.code === 'ENOENT' ? 404 : 500);
+      res.end(err.code === 'ENOENT' ? 'Not Found' : 'Internal Server Error');
+      return;
+    }
+    const ext = path.extname(filePath);
+    res.writeHead(200, { 'Content-Type': MIME_TYPES[ext] || 'application/octet-stream' });
+    res.end(data);
+  });
 });
 
 server.listen(PORT, () => {
