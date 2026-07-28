4ed6aa79b4aeddaa3df3c40541d88537f572837f
Aleksandr Tapinsh <squrtle2003@gmail.com>
2026-07-27 21:31:31 -0400

    change chat refreshing

--- chat.js
+++ chat.js
@@ -102,9 +102,6 @@
         log.scrollTop = log.scrollHeight;
       } else if (payload.type === 'message') {
         addMessage(payload);
-      } else if (payload.type === 'cleared') {
-        log.textContent = '';
-        systemLine('Chat was cleared.');
       } else if (payload.type === 'error') {
         systemLine(payload.message);
       }
